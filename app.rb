67ac539f3bf7eab72772379c6d6006b8aceae4b5
Dane Hammer <dane.molotok@gmail.com>
2015-02-23 20:25:22 -0600

    make modules configurable

--- app.rb
+++ app.rb
@@ -1,14 +1,16 @@
 require 'sinatra'
+require 'yaml'
 
 require './helpers'
 
-require './modules/lights'
-require './modules/temperature'
-require './modules/iRiver_player'
+# allow configurable modules, only require what's desired
+modules = YAML.load_file('config.yml')['modules']
+require './modules/lights' if modules.include? 'hue'
+require './modules/temperature' if modules.include? 'nest'
+require './modules/iRiver_player' if modules.include? 'iriver'
 
 require 'numbers_in_words'
 require 'numbers_in_words/duck_punch'
-
 
 def process_query(command)
   # HUE LIGHTS #
@@ -18,7 +20,7 @@ def process_query(command)
   elsif command.scan(/temperature|nest/).length > 0
     process_temperature(command)
   elsif command.scan(/river/).length > 0
-    process_player(command, player: "iriver")
+    process_player(command, player: 'iriver')
   end
 end
 
--- startupScript.sh
+++ startupScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/pi/dev/alexa-home
 source ~/.env
+cd $ALEXA_HOME
 bundle exec ruby app.rb &
 bundle exec ruby watir-login.rb &
